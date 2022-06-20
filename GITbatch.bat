8cb2424a7d987d785c753ad08904ddd1428ae116
Amrapali12 <106974309+Amrapali12@users.noreply.github.com>
2022-06-20 15:22:42 +0530

    Update GITbatch.bat

--- GITbatch.bat
+++ GITbatch.bat
@@ -1,4 +1,4 @@
 @echo off
 
-powershell -Command "Compress-Archive -path C:\Files\*.html -DestinationPath C:\Test\files.zip"
+powershell -Command "Compress-Archive -path C:/Files/*.html -DestinationPath C:/Test/files.zip"
 pause
